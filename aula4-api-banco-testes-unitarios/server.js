76d7e5a2fc7782638ccaad912ba03327b6e5818a
CamilaCSilva <90875966+CamilaCSilva@users.noreply.github.com>
2022-10-09 17:00:26 -0300

    corrigindo o commit

--- aula4-api-banco-testes-unitarios/server.js
+++
@@ -1,9 +0,0 @@
-require('./src/infrastructure/database');
-const app = require('./src/infrastructure/rest_server');
-
-app.listen(3000, (err) => {
-    if(err) {
-        return console.error(err);
-    }
-    console.log('REST server running on port 3000...');
-});
